e271b4e46deba25c4deb6c1ce5dc15b08c6d689a
chaidaichen <chaidaichen@corp.netease.com>
2018-04-09 20:47:11 +0800

    add a webserver test

--- sapphiredb/build/raft/spdlog/spdlog/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/cdcupt/cdcupt/sapphiredb/sapphiredb/common/spdlog
-# Build directory: /Users/cdcupt/cdcupt/sapphiredb/sapphiredb/build/raft/spdlog/spdlog
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
